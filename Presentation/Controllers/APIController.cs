3b10e84d01de6119ffe22623da4e43533a081710
Petter Karlsrud <petterkarlsrud@me.com>
2021-09-23 15:42:27 +0200

    Initial

---
+++ Presentation/Controllers/APIController.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Template.Application.Commands;
+
+namespace Template.Presentation.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class APIController : ControllerBase
+    {
+        private readonly ILogger<APIController> _logger;
+        private readonly IMediator _mediator;
+
+        public APIController(ILogger<APIController> logger, IMediator mediator)
+        {
+            _logger = logger;
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            string result = await _mediator.Send(new PingCommand());
+            _logger.LogInformation("returning {Result}", result);
+            return Ok(result);
+        }
+    }
+}
---
+++ Presentation/Program.cs
@@ -0,0 +1,38 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+
+namespace Template.API
+{
+    public class Program
+    {
+        private static string ApplicationName = nameof(Template.API);
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateBootstrapLogger();
+            try
+            {
+                Log.Information("{ApplicationName} is starting", ApplicationName);
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "{ApplicationName} failed to start", ApplicationName);
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
+    }
+}
