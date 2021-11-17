3de05e820defe42a358759a58e5be677e5da1b86
Petter Karlsrud <petterkarlsrud@me.com>
2021-11-17 18:24:01 +0100

    Started creating integration tests

---
+++ Presentation.Tests/WebApplicationFactoryTests.cs
@@ -0,0 +1,27 @@
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace Template.API.Application.Tests
+{
+    public class WebApplicationFactoryTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public WebApplicationFactoryTests(WebApplicationFactory<Startup> factory) => _factory = factory;
+
+        [Theory]
+        [InlineData("/API")]
+        public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
+        {
+            HttpClient client = _factory.CreateClient();
+            HttpResponseMessage response = await client.GetAsync(url);
+
+            response.EnsureSuccessStatusCode();
+
+            if (response.Content.Headers.ContentType != null)
+                Assert.Equal("application/json; charset=utf-8", response.Content.Headers.ContentType.ToString());
+        }
+    }
+}
