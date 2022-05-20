d2a14623b49120896d291ba9a180e702321aeb0e
ptrkrlsrd <ptrkrlsrd@users.noreply.github.com>
2022-05-20 11:34:23 +0200

    Update PingQuery.cs

--- Application/Queries/PingQuery.cs
+++ Application/Queries/PingQuery.cs
@@ -3,15 +3,14 @@ using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
 
-namespace Template.API.Application.Queries
-{
-    public class PingQuery : IRequest<string> { }
+namespace Template.API.Application.Queries;
 
-    public class PingQueryHandler : IRequestHandler<PingQuery, string>
+public class PingQuery : IRequest<string> { }
+
+public class PingQueryHandler : IRequestHandler<PingQuery, string>
+{
+    public Task<string> Handle(PingQuery request, CancellationToken cancellationToken)
     {
-        public Task<string> Handle(PingQuery request, CancellationToken cancellationToken)
-        {
-            return Task.FromResult("Pong");
-        }
+        return Task.FromResult("Pong");
     }
 }
